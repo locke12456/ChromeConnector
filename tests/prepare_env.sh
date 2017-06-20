2799137ab0f8b62ff1b1d9cfea2dd1bc3456a5ff
Locke Chen <locke12456@gmail.com>
2017-06-20 17:50:32 +0800

    added setup debug env script.

---
+++ tests/prepare_env.sh
@@ -0,0 +1,7 @@
+export HOME="$(realpath ~)"
+export MOZ_HOME="$HOME/src/mozilla-central"
+export NETMONITOR_HOME="$MOZ_HOME/devtools/client/netmonitor"
+gnome-terminal --working-directory="$MOZ_HOME"
+gnome-terminal -e "google-chrome --remote-debugging-port=9222 -v"
+gnome-terminal --working-directory="$MOZ_HOME" -e "./mach run http://localhost:8000 --start-debugger-server 6080"
+gnome-terminal --working-directory="$NETMONITOR_HOME" -e "yarn start"
